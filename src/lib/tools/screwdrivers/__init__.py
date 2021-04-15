3b595005314c6c42308fa5150267befbd964c3cf
LanguidYouth <1504188+danieltorrey@users.noreply.github.com>
2021-04-15 11:49:11 -0400

    Add gradle

---
+++ src/lib/tools/screwdrivers/__init__.py
@@ -0,0 +1,5 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+log.debug('{0} {1}'.format('(__init__.py) hello, world from ', __name__))
---
+++ src/lib/tools/screwdrivers/screwdrivers.py
@@ -0,0 +1,40 @@
+import logging
+from lib.utils import debugutils
+
+log = logging.getLogger(__name__)
+
+log.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+fmt = logging.Formatter('%(asctime)-15s %(name)-10s %(levelname)-8s :%(message)s')
+ch.setFormatter(fmt)
+log.addHandler(ch)
+log.debug('{0} {1}:{2}'.format('hello, world from ', __name__, debugutils.get_linenumber()))
+
+
+class ScrewdriverBase:
+
+    def __init__(self, screwdrivertype='FLATHEAD'):
+        self._screwdrivertype = screwdrivertype
+
+    @property
+    def screwdrivertype(self):
+        return self._screwdrivertype
+
+class FlatheadScrewdriver(ScrewdriverBase):
+
+    def __init__(self):
+        super(FlatheadScrewdriver, self).__init__()
+
+class PhillipsScrewdriver(ScrewdriverBase):
+
+    def __init__(self):
+        super(PhillipsScrewdriver, self).__init__('PHILLIPS')
+
+
+class TorxScrewdriver(ScrewdriverBase):
+
+    def __init__(self):
+        super(TorxScrewdriver, self).__init__('TORX')
+
