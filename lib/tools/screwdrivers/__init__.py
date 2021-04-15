14b7879cb02ec4e289cdad7d5eedfbdae80a504d
LanguidYouth <1504188+danieltorrey@users.noreply.github.com>
2021-04-15 11:53:00 -0400

    Complete move of src tree

--- lib/tools/screwdrivers/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-log = logging.getLogger(__name__)
-
-log.debug('{0} {1}'.format('(__init__.py) hello, world from ', __name__))
--- lib/utils/debugutils.py
+++
@@ -1,6 +0,0 @@
-from inspect import currentframe
-
-
-def get_linenumber():
-    cf = currentframe()
-    return cf.f_back.f_lineno
